2cbcba11d67fcd4a0993936fbbfb4014ab4a2dd5
Brian Barros <brian.ferreira@opikode.com>
2024-10-27 17:58:51 -0300

    Create login and logout flow

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,39 +2,46 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Services\LoginService;
+use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
     /**
-     * Where to redirect users after login.
+     * Create a new LoginController instance.
      *
-     * @var string
+     * @param \App\Services\LoginService $loginService
      */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function __construct(protected LoginService $loginService)
     {
-        $this->middleware('guest')->except('logout');
-        $this->middleware('auth')->only('logout');
+        // ...
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param \App\Http\Requests\LoginRequest $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        $redirect = $this->loginService->login($request->validated());
+
+        return redirect()->intended($redirect);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout(): RedirectResponse
+    {
+        $this->loginService->logout();
+
+        return redirect('/');
     }
 }
